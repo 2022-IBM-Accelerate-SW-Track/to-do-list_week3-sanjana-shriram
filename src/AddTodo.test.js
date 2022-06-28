caac181c2be5a1a95ad717b66eb8a909017ae829
S S <sanjanashriram2012@gmail.com>
2022-06-27 22:13:21 -0500

    all tests

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -83,21 +83,34 @@ afterEach(() => {
   const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
   const element = screen.getByRole('button', {name: /Add/i});
   const dueDate = "09/22/2020";
+  const dueDate2 = "09/22/2024";
+
   fireEvent.change(inputTask, { target: { value: "History Test"}});
   fireEvent.change(inputDate, { target: { value: dueDate}});
   fireEvent.click(element);
+  fireEvent.change(inputTask, { target: { value: "Enhypen"}});
+  fireEvent.change(inputDate, { target: { value: dueDate2}});
+  fireEvent.click(element);
   
-  const inputTask2 = screen.getByRole('textbox', {name: /Add New Item/i});
-  const inputDate2 = screen.getByPlaceholderText("mm/dd/yyyy");
-  const element2 = screen.getByRole('button', {name: /Add/i});
-  const dueDate2 = "09/22/2023";
-  fireEvent.change(inputTask2, { target: { value: "Math Test"}});
-  fireEvent.change(inputDate2, { target: { value: dueDate}});
-  fireEvent.click(element2);
+  const historyCheck = screen.getByTestId(/History Test/i).style.background;
+  const enhypenCheck = screen.getByTestId(/Enhypen/i).style.background;
+  expect(historyCheck).toEqual("rgb(255, 0, 0)");
+  expect(enhypenCheck).toEqual("rgb(255, 255, 255)");
+  // Check and remove task
+  // const check = screen.getByRole('checkbox');
+  // expect(check).equalsTo("rgb(255,255,255");
+  // fireEvent.click(check);
+
+  // const inputTask2 = screen.getByRole('textbox', {name: /Add New Item/i});
+  // const inputDate2 = screen.getByPlaceholderText("mm/dd/yyyy");
+  // const element2 = screen.getByRole('button', {name: /Add/i});
+  // const dueDate2 = "09/22/2023";
+  // fireEvent.change(inputTask2, { target: { value: "Math Test"}});
+  // fireEvent.change(inputDate2, { target: { value: dueDate}});
+  // fireEvent.click(element2);
   
-  
-  const checkBoxTask = screen.getByRole('checkbox');
-  fireEvent.click(checkBoxTask);
-  const check = screen.getByText(/You have no todo's left/i);
-  expect(check).toBeInTheDocument();
+  // const check2 = screen.getByRole('checkbox');
+  // expect(check2).equalsTo("rgb(255,255,255");
+
+
  });
